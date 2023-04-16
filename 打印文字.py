6e5a4de42366f8d7cecb034d91cee4218f2c5200
cn <121707519+examinationfirst@users.noreply.github.com>
2023-04-16 09:44:15 +0800

    Create 打印文字.py

---
+++ 打印文字.py
@@ -0,0 +1,11 @@
+print(
+"Hello World!"
+"Number is 198"
+"http://c.biancheng.net/python/"
+);
+print("Hello World!" "Python is great!" "Number is 198.")
+print(
+"Hello World! "
+"Number is 198 "
+"http://c.biancheng.net/python/"
+);
